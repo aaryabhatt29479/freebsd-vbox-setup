af879f17211aec175a34dcef92aa733d65fa5ce2
aaryabhatt29479 <131638015+aaryabhatt29479@users.noreply.github.com>
2025-08-05 08:02:12 +0800

    setup-freebsd-desktop.sh

--- setup-freebsd-desktop.sh
+++ setup-freebsd-desktop.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-
-# Exit on error
 set -e
 
-echo "Updating pkg..."
+echo "Updating pkg repositories..."
 pkg update && pkg upgrade -y
 
 echo "Installing VirtualBox Guest Additions..."
 pkg install -y virtualbox-ose-additions
 
-echo "Enabling VirtualBox Guest Additions services..."
+echo "Enabling VBox services..."
 sysrc vboxguest_enable=YES
 sysrc vboxservice_enable=YES
 
@@ -22,7 +20,8 @@ sysrc hald_enable=YES
 sysrc sddm_enable=YES
 
 echo "Adding user to wheel and video groups..."
-pw groupmod wheel -m mj29479
-pw groupmod video -m mj29479
+pw groupmod wheel -m yourusername
+pw groupmod video -m yourusername
 
 echo "Setup Complete. Please reboot now."
+
